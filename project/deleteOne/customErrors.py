ad0edf358a9527a79d248d203b9311af8e3ea521
knotkin <knotkin@owneriq.com>
2017-09-07 15:10:30 -0400

    deleteOne lambda; customErrors

---
+++ project/deleteOne/customErrors.py
@@ -0,0 +1,7 @@
+# add custom errors as you go
+customErrors={
+'COLLECTION': '{"ERROR":"Invalid or empty collection"}',
+'INVALIDID': '{"ERROR":"Invalid ObjectId"}',
+'MISSINGARG': '{"ERROR":"Missing argument key, possibly missing _id key"}',
+'FAILEDDELETE': '{"ERROR":"Failed delete"}',
+}
